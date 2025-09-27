40aa0bb0ba23f8f0e0aa175a29d679ca3f2c7b6d
smatsuodev <git@smatsuo.dev>
2025-09-28 01:42:09 +0900

    fix

--- webapp/backend/internal/repository/session.go
+++ webapp/backend/internal/repository/session.go
@@ -14,40 +14,41 @@ type SessionRepository struct {
 }
 
 func NewSessionRepository(db DBTX) *SessionRepository {
-	go bulkInsertSession(db)
+	// TODO: バルクインサートしたい
+	// go bulkInsertSession(db)
 	return &SessionRepository{db: db}
 }
 
-type Session struct {
-	sessionID string    `db:"session_id"`
-	userID    int       `db:"user_id"`
-	expiredAt time.Time `db:"expires_at"`
-}
+// type Session struct {
+// 	sessionID string    `db:"session_id"`
+// 	userID    int       `db:"user_id"`
+// 	expiredAt time.Time `db:"expires_at"`
+// }
 
-var sessionCh = make(chan *Session, 1200)
+// var sessionCh = make(chan *Session, 1200)
 
-func bulkInsertSession(db DBTX) {
-	var values [1200]*Session
-	ticker := time.NewTicker(10 * time.Second)
-	i := 0
-	for {
-		if i > 1000 {
-			query := "INSERT INTO user_sessions (session_uuid, user_id, expires_at) VALUES (:session_id, :user_id, :expires_at)"
-			_, _ = db.NamedExec(query, values[0:i+1])
-			i = 0
-		}
-		select {
-		case <-ticker.C:
-			if i > 0 {
-				query := "INSERT INTO user_sessions (session_uuid, user_id, expires_at) VALUES (:session_id, :user_id, :expires_at)"
-				_, _ = db.NamedExec(query, values[0:i+1])
-				i = 0
-			}
-		case values[i] = <-sessionCh:
-			i++
-		}
-	}
-}
+// func bulkInsertSession(db DBTX) {
+// 	var values [1200]*Session
+// 	ticker := time.NewTicker(500 * time.Millisecond)
+// 	i := 0
+// 	for {
+// 		if i > 1000 {
+// 			query := "INSERT INTO user_sessions (session_uuid, user_id, expires_at) VALUES (:session_id, :user_id, :expires_at)"
+// 			_, _ = db.NamedExec(query, values[0:i+1])
+// 			i = 0
+// 		}
+// 		select {
+// 		case <-ticker.C:
+// 			if i > 0 {
+// 				query := "INSERT INTO user_sessions (session_uuid, user_id, expires_at) VALUES (:session_id, :user_id, :expires_at)"
+// 				_, _ = db.NamedExec(query, values[0:i+1])
+// 				i = 0
+// 			}
+// 		case values[i] = <-sessionCh:
+// 			i++
+// 		}
+// 	}
+// }
 
 // セッションを作成し、セッションIDと有効期限を返す
 func (r *SessionRepository) Create(ctx context.Context, userBusinessID int, duration time.Duration) (string, time.Time, error) {
@@ -58,13 +59,18 @@ func (r *SessionRepository) Create(ctx context.Context, userBusinessID int, dura
 	expiresAt := time.Now().Add(duration)
 	sessionIDStr := sessionUUID.String()
 
+	query := "INSERT INTO user_sessions (session_uuid, user_id, expires_at) VALUES (?, ?, ?)"
+	_, err = r.db.ExecContext(ctx, query, sessionIDStr, userBusinessID, expiresAt)
+	if err != nil {
+		return "", time.Time{}, err
+	}
 	cache.Cache.Session.Lock()
 	cache.Cache.Sessions[sessionIDStr] = struct {
 		UserID    int
 		ExpiresAt time.Time
 	}{userBusinessID, expiresAt}
 	cache.Cache.Session.Unlock()
-	sessionCh <- &Session{sessionIDStr, userBusinessID, expiresAt}
+	// sessionCh <- &Session{sessionIDStr, userBusinessID, expiresAt}
 	return sessionIDStr, expiresAt, nil
 }
 
